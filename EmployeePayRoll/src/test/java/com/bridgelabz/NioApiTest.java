873c3a77cf6e9fb59895e9372268ff97405ed882
Vivek varshney <Vivek.iitd93@gmail.com>
2021-08-06 23:13:06 +0530

    Ablity to create a watch services to watch particular dir

--- EmployeePayRoll/src/test/java/com/bridgelabz/NioApiTest.java
+++ EmployeePayRoll/src/test/java/com/bridgelabz/NioApiTest.java
@@ -10,28 +10,26 @@ import java.util.stream.IntStream;
 
 import org.junit.Test;
 
+// import employeepayrollservice.Java8WatchServiceExample;
+
 public class NioApiTest {
     private static String HOME = System.getProperty("user.home");
     private static String PLAY_WITH_NIO = "TempPlayGround";
 
     @Test
     public void givenPathWhenCHeckedThenConfirm() throws IOException {
-        //check file exists
         Path homePath = Paths.get(HOME);
         assertTrue(Files.exists(homePath));
 
-        //Delete file and check file not Exists
         Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
         if (Files.exists(playPath)) {
             Files.delete(playPath);
         }
         assertTrue(Files.notExists(playPath));
 
-        //create directory
         Files.createDirectory(playPath);
         assertTrue(Files.exists(playPath));
 
-        //create file
         IntStream.range(1, 10).forEach(cntr -> {
             Path tempFile = Paths.get(playPath + "/temp" + cntr);
             assertTrue(Files.notExists(tempFile));
@@ -43,13 +41,10 @@ public class NioApiTest {
             assertTrue(Files.exists(tempFile));
         });
 
-        //list files,directories as well as files with extension
-        // listing files
         Files.list(playPath)
                 .filter(Files::isRegularFile)
                 .forEach(System.out::println);
 
-        //listing directories
         Files.newDirectoryStream(playPath).forEach(System.out::println);
 
         Files.newDirectoryStream(playPath,
@@ -57,6 +52,13 @@ public class NioApiTest {
                         path.toString().startsWith("temp"))
                 .forEach(System.out::println);
 
+    }
+
+    @Test
+    public void givenADirectoryWhenWatchedListAllActivities() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExample(dir).processEvents();
     }
 
 }
