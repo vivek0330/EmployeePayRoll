47f0f014ce4f4ce536c16c9a72119b4dd10417a7
Vivek varshney <Vivek.iitd93@gmail.com>
2021-08-06 22:53:00 +0530

    Initial comment

---
+++ EmployeePayRoll/src/main/java/com/bridgelabz/EmployeePayRoll.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeePayRoll {
+    public static void main(String[] args) {
+        System.out.println("Welcome employee Pay Roll Services Problem");
+    }
+}
